fc10ef9bcdb46989472a1cbe3fc765bd61f07403
Abidiel Bulsing <52458866+abidiel@users.noreply.github.com>
2025-07-10 17:26:28 -0300

    Ajustar tamanho breadcrumb

--- themes/trendpro (revisado)/template-parts/breadcrumbs.php
+++ themes/trendpro (revisado)/template-parts/breadcrumbs.php
@@ -8,7 +8,7 @@ $imagem_breadcrumb = wp_get_attachment_image_src($attachment_breadcrumb_id, $siz
 
 <section class="page-title-big-typography bg-dark-gray cover-background background-position-center-bottom p-0" style="background-image: url(<?php echo $imagem_breadcrumb[0]; ?>)">
     <div class="container">
-        <div class="row align-items-end justify-content-center small-screen md-small-screen sm-extra-small-screen pb-9">
+        <div class="row align-items-end justify-content-center extra-very-small-screen md-extra-very-small-screen sm-extra-very-small-screen pb-1">
             <div class="col-lg-6 col-md-8 position-relative page-title-extra-small text-center" data-anime='{ "el": "childs", "translateY": [30, 0], "opacity": [0,1], "duration": 600, "delay": 0, "staggervalue": 300, "easing": "easeOutQuad" }'>
                 <h4 class="mb-20px primary-font text-white fw-700"><?php the_title(); ?></h4>
                 <!-- <h2 class="text-white mb-0 text-uppercase ls-3px fw-600">Conheça</h2> -->
